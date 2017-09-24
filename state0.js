631ac9ef15575b65d9b5d01020d08ba72fbcea33
unknown <robson.shimada@gmail.com>
2017-09-24 15:29:44 -0300

    finish state0

--- state0.js
+++ state0.js
@@ -1,29 +1,55 @@
-var demo = {}, centerX = 1500 / 2, centerY = 1000 / 2, adam, speed = 4;
-var centerX = 1500/2;
-var centerY = 1000/2;
+var demo = {}, centerX = 1500 / 2, centerY = 1000 / 2, adam, speed = 6;//declara as variaiveis
+var centerX = 1500/2;//variável de posição x
+var centerY = 1000/2;//variável de posição y
 demo.state0 = function () {};
 demo.state0.prototype = {
     preload: function () {
-        game.load.image('adam', 'assets/sprites/Adam.png');
+        game.load.spritesheet('adam', 'assets/spritesheets/adamSheet.png', 240, 370);
+        /*load a imagem de sprite, depois de Adam.png 
+        trocamos para o spritesheet para adiiconar movimento, adicionou 240 e 370 que são as dimensões da imagem*/
+        game.load.image('tree', 'assets/backgrounds/treeBG.png');//load a imagem de background floresta
     },
     create: function () {
-        game.stage.backgroundColor = '#808080';
-        console.log('state0');
-        addChangeStateEventListeners();
-        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        game.physics.startSystem(Phaser.Physics.ARCADE);//ativa a Física no jogo
+        game.stage.backgroundColor = '#808080';//fundo da tela original state 0
+        console.log('state0');//diz no console que está no state 0
+        game.world.setBounds(0, 0, 2813, 1000);//coloca as fronteiras do nosso mundo que é o comprimento da imagem de background
+        addChangeStateEventListeners();//coloca ativadores de eventos
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;//ajusta tamanho da tela
+        var treeBG = game.add.sprite(0,0,'tree');//variável local de background
+        adam = game.add.sprite(centerX, centerY, 'adam');//adiciona o sprite na tela
+        adam.anchor.setTo(0.5,0.5);//coloca ele no meio da tela
+        adam.scale.setTo(0.7, 0.7);//diminiu a escala do personagem, truque de virar de lado é colocar negativo no 1º valor
+        game.physics.enable(adam);//libera a Física para o adam
+        adam.body.collideWorldBounds = true;//faz com que o adam para de andar quando atinge a fronteira do mundo
+        adam.animations.add('walk', [0,1,2,3,4]);
         
-        adam = game.add.sprite(centerX, centerY, 'adam');
-        adam.anchor.setTo(0.5,0.5);
+        game.camera.follow(adam);//pede pra camera seguir o adam
+        game.camera.deadzone = new Phaser.Rectangle(centerX - 300, 0, 600, 1000);//colocar onde é a nossa zona morta
+        
+        
     },
-    update: function () {
+    update: function () { //aqui, você tem a movimentação do personagem na tela de acordo com os eventos de tecla pressionada
         if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+            adam.scale.setTo(0.7, 0.7);//ele vira na direção normal quando pressiona direita
             adam.x += speed;
+            adam.animations.play('walk', 14, true);//14 é framerate
         }
         else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+            adam.scale.setTo(-0.7, 0.7);//quando coloca negativo ele vira de lado quand pressiona tecla esquerda
             adam.x -= speed;
+            adam.animations.play('walk', 14, true);
         }
+        else {
+            adam.animations.stop('walk');//este else resolve o problema da animação continuar com o adam parado
+            adam.frame = 0;
+        }
+            
         if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
             adam.y -= speed;
+            if(adam.y < 395){//faz com que o adam não ultrapasse a estradinha de terra
+                adam.y = 395;
+            }
         }
          else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
             adam.y += speed;
